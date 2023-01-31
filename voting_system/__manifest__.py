fa0405e03f6ef2677dd572ac9b0ae625cc638b55
Om Rabara <omra@odoo.com>
2023-01-31 19:01:04 +0530

    [IMP] voting_module: complete pdf reports and ACL
    Implement the report for voter's voting details
    implement the access rules for all the models .

--- voting_system/__manifest__.py
+++ voting_system/__manifest__.py
@@ -1,14 +1,18 @@
 # -- coding: utf-8 --
 {
     'name':'voting_system',
+    'category': 'Voting System/election',
     'author':'om rabara',
     'data':[
         'security/ir.model.access.csv',
+        'security/security.xml',
         'views/voter_view.xml',
         'views/voting_menu.xml',
         'views/candidate_view.xml',
         'views/party_view.xml',
         'views/area_view.xml',
+        'report/voter_model_report.xml',
+        'report/voter_model_templates.xml',
         
     ],
     'depends': ['mail','base'],
